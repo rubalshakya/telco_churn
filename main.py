955d29185c86c6028064e97a21ac681555d63f6d
Rubal Shakya <rubalshakya@gmail.com>
2023-03-31 23:37:10 +0530

    add

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def Predict_churn():
     print(res)
     return render_template("result.html",Result = res)
 
-app.run(debug=True)
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port = "8080")
     
 
 
